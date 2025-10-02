4f765953aec5ebb21c7cfec1f474178f70dabbb5
Pranjal Tripathi <155900440+PranjalTripatHI07@users.noreply.github.com>
2025-10-03 01:39:50 +0530

    Update Window_function_tutorial_code.sql

--- SQL Tutorial_V_Code/Window_function_tutorial_code.sql
+++ SQL Tutorial_V_Code/Window_function_tutorial_code.sql
@@ -1,1 +1,20 @@
+/** Topic -> Window Function **/
 
+
+-- Using Row_number() ranking window function  
+
+
+select emp_no,
+       salary, 
+       row_number() over([partition by emp_no] order by salary desc) as row_num
+
+from salaries; 
+
+
+--  Using Row_number() ranking window function with empty over clause
+
+select emp_no,
+       salary,
+       row_number() over() as row_num
+
+from salaries;
