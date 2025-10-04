cd674a4e40172e686da5896a03058c0892a43e75
Pranjal Tripathi <155900440+PranjalTripatHI07@users.noreply.github.com>
2025-10-04 19:33:44 +0530

    Update Window_function_tutorial_code.sql

--- SQL Tutorial_V_Code/Window_function_tutorial_code.sql
+++ SQL Tutorial_V_Code/Window_function_tutorial_code.sql
@@ -6,7 +6,7 @@
 
 select emp_no,
        salary, 
-       row_number() over([partition by emp_no] order by salary desc) as row_num
+       row_number() over(partition by emp_no order by salary desc) as row_num
 
 from salaries; 
 
@@ -18,3 +18,40 @@ select emp_no,
        row_number() over() as row_num
 
 from salaries;
+
+
+
+-- Multiple row_number ranking window function
+
+select emp_no,
+       salary,
+       row_number() over(partition by emp_no) as row_num1,
+       row_number() over(partition by emp_no order by salary desc) as row_num2
+
+from salaries;
+
+
+
+-- Exercise 
+
+select dm.emp_no,
+       salary, 
+       row_number() over(partition by emp_no) as row_num1,
+       row_number() over(partition by emp_no order by salary asc) as row_num2
+
+from dept_manager as dm
+inner join salaries as s
+on dm.emp_no = s.emp_no 
+order by row_num1, emp_no, salary asc;
+
+
+
+SELECT dm.emp_no,
+       salary,
+       ROW_NUMBER() OVER (PARTITION BY emp_no ORDER BY salary ASC) AS row_num1,
+       ROW_NUMBER() OVER (PARTITION BY emp_no ORDER BY salary DESC) AS row_num2   
+
+FROM dept_manager dm
+INNER JOIN salaries s 
+ON dm.emp_no = s.emp_no;
+
