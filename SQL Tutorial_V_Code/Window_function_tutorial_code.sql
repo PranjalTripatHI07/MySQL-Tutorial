e0c68904f69a90a5160fd9593afad9058060fe94
Pranjal Tripathi <155900440+PranjalTripatHI07@users.noreply.github.com>
2025-10-07 18:43:22 +0530

    Update Window_function_tutorial_code.sql

--- SQL Tutorial_V_Code/Window_function_tutorial_code.sql
+++ SQL Tutorial_V_Code/Window_function_tutorial_code.sql
@@ -55,3 +55,74 @@ FROM dept_manager dm
 INNER JOIN salaries s 
 ON dm.emp_no = s.emp_no;
 
+
+-- Partition by clause vs Group by clause 
+
+-- Exercise 1
+-- sol 1
+select temp.emp_no,
+       min(salary) as lowest_salary
+from (select emp_no,
+             salary, 
+             row_number() over w as row_num
+       from salaries
+       window w as (Partition by emp_no order by salary desc)) as temp
+
+group by emp_no;
+
+
+-- Exercise 2
+-- sol 2
+
+select temp.emp_no,
+       min(salary) as lowest_salary
+from (select emp_no,
+             salary,
+             row_number() over(Partition by emp_no order by salary desc) as row_num
+      from salaries) as temp
+
+group by emp_no;
+
+
+-- Exercise 4 
+-- sol 4
+-- without using window function 
+select temp.emp_no,
+       min(salary) as lowest_salary
+from (select emp_no,
+             salary
+       from salaries ) as temp
+
+group by emp_no;
+
+
+-- Exercise 5
+-- sol 5 
+-- Here we obtain output without using group by clause instead we use 
+-- where clause 
+
+select temp.emp_no,
+       temp.salary as lowest_salary
+from (select emp_no,
+             salary,
+             row_number() over w as row_num 
+       from salaries
+       window w as (Partition by emp_no order by salary desc)) as temp
+
+where temp.row_num = 1;
+
+
+-- Exercise 6
+-- sol 6 
+-- here we are finding second lowest salary of employee without using group by clause
+
+select temp.emp_no,
+       temp.salary as lowest_salary
+from (select emp_no,
+             salary,
+             row_number() over w as row_num 
+       from salaries
+       window w as (Partition by emp_no order by salary desc)) as temp
+
+where temp.row_num = 2;
+
