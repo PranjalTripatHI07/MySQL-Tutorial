1c418836a952b9a215cc5e5b477b97f302154667
Pranjal Tripathi <155900440+PranjalTripatHI07@users.noreply.github.com>
2025-10-26 17:41:48 +0530

    Add CTE example for male employees' salaries
    Add example using multiple CTEs to calculate the number of male employees with highest salaries below the average.

--- SQL Tutorial_V_Code/Window_function_tutorial_code.sql
+++ SQL Tutorial_V_Code/Window_function_tutorial_code.sql
@@ -910,3 +910,42 @@ from salaries as s
 inner join employess as e
 on s.emp_no = e.emp_no and e.gender = "M" 
 inner join cte as c;
+
+
+
+
+
+-- Example  for using multiple ctes 
+/** 
+Use two common table expressions and a SUM() function in the SELECT 
+statement of a query to obtain the number of male employees whose 
+highest salaries have been below the all-time average.
+**/
+
+WITH cte1 AS (
+
+SELECT AVG(salary) AS avg_salary FROM salaries
+
+),
+
+cte2 AS (
+
+SELECT s.emp_no, MAX(s.salary) AS max_salary
+
+FROM salaries s
+
+JOIN employees e ON e.emp_no = s.emp_no AND e.gender = 'M'
+
+GROUP BY s.emp_no
+
+)
+
+SELECT
+
+SUM(CASE WHEN c2.max_salary < c1.avg_salary THEN 1 ELSE 0 END) AS highest_salaries_below_avg
+
+FROM employees e
+
+JOIN cte2 c2 ON c2.emp_no = e.emp_no
+
+JOIN cte1 c1;
